8762fe24dea69602f95b940f6aebde82c544bfbc
ismanouman <isn192525@gmail.com>
2023-11-01 21:08:47 +0500

    Initial commit

---
+++ face recognition.py
@@ -0,0 +1,41 @@
+import face_recognition
+import cv2
+import os
+
+known_faces_dir = "known_faces"
+known_faces = []
+
+for filename in os.listdir(known_faces_dir):
+    if filename.endswith(".jpeg"):
+        image_path = os.path.join(known_faces_dir, filename)
+        image = face_recognition.load_image_file(image_path)
+        encoding = face_recognition.face_encodings(image)[0]
+        known_faces.append(encoding)
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = video_capture.read()
+
+    face_locations = face_recognition.face_locations(frame)
+    face_encodings = face_recognition.face_encodings(frame, face_locations)
+
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        matches = face_recognition.compare_faces(known_faces, face_encoding)
+        name = "Unknown"
+
+        if True in matches:
+            first_match_index = matches.index(True)
+            name = os.listdir(known_faces_dir)[first_match_index].split(".")[0]
+
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
+
+    cv2.imshow("Video", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
